c44c24cc1ec7399fd2c1bee8b78cecc353b2a9b7
swapnilRoyWeb <swapnilroyweb@gmail.com>
2023-05-03 21:50:24 +0600

    chef data loaded and specific chef can be selected

--- index.js
+++ index.js
@@ -3,11 +3,23 @@ const cors = require('cors');
 const app = express();
 const port = 4000;
 
+const chefs = require('./data/chef.json');
+
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Chef is running')
 });
+
+app.get('/chef', (req, res) => {
+    res.send(chefs);
+})
+
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedChef = chefs.find(c => c.id == id);
+    res.send(selectedChef);
+})
 
 app.listen(port, () => {
     console.log(`Chef is running on port ${port}`)
